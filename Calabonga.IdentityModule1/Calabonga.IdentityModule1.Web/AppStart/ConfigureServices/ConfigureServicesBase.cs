7813d8fbecdb93ac3ca963c163ecbebbc94d6723
Сергей Захаров <sergei86zahar@mail.ru>
2021-08-11 13:36:39 +0700

    поменял строку подключения

--- Calabonga.IdentityModule1/Calabonga.IdentityModule1.Web/AppStart/ConfigureServices/ConfigureServicesBase.cs
+++ Calabonga.IdentityModule1/Calabonga.IdentityModule1.Web/AppStart/ConfigureServices/ConfigureServicesBase.cs
@@ -30,13 +30,13 @@ namespace Calabonga.IdentityModule1.Web.AppStart.ConfigureServices
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
-            services.AddDbContextPool<ApplicationDbContext>(config =>
+            services.AddDbContext<ApplicationDbContext>(config =>
             {
                 // UseInMemoryDatabase - This for demo purposes only!
                 // Should uninstall package "Microsoft.EntityFrameworkCore.InMemory" and install what you need. 
                 // For example: "Microsoft.EntityFrameworkCore.SqlServer"
                 // uncomment line below to use UseSqlServer(). Don't forget setup connection string in appSettings.json 
-                config.UseInMemoryDatabase("DEMO_PURPOSES_ONLY");
+                config.UseSqlite(configuration.GetConnectionString(nameof(ApplicationDbContext)));
                 // config.UseSqlServer(configuration.GetConnectionString(nameof(ApplicationDbContext)));
             });
 
