48d951d15762a822e392265192a4d74aabbd4c58
Сергей Захаров <sergei86zahar@mail.ru>
2021-08-15 19:59:29 +0700

    1

--- Calabonga.IdentityModule1/Calabonga.IdentityModule1.Web/Infrastructure/Mappers/Base/PagedListConverter.cs
+++ Calabonga.IdentityModule1/Calabonga.IdentityModule1.Web/Infrastructure/Mappers/Base/PagedListConverter.cs
@@ -10,7 +10,8 @@ namespace Calabonga.IdentityModule1.Web.Infrastructure.Mappers.Base
     /// </summary>
     /// <typeparam name="TMapFrom"></typeparam>
     /// <typeparam name="TMapTo"></typeparam>
-    public class PagedListConverter<TMapFrom, TMapTo> : ITypeConverter<IPagedList<TMapFrom>, IPagedList<TMapTo>>
+    public class PagedListConverter<TMapFrom, TMapTo> : 
+        ITypeConverter<IPagedList<TMapFrom>, IPagedList<TMapTo>>
     {
 
         /// <summary>Performs conversion from source to destination type</summary>
@@ -18,13 +19,18 @@ namespace Calabonga.IdentityModule1.Web.Infrastructure.Mappers.Base
         /// <param name="destination">Destination object</param>
         /// <param name="context">Resolution context</param>
         /// <returns>Destination object</returns>
-        public IPagedList<TMapTo> Convert(IPagedList<TMapFrom> source, IPagedList<TMapTo> destination, ResolutionContext context)
+        public IPagedList<TMapTo> Convert(
+            IPagedList<TMapFrom> source,
+            IPagedList<TMapTo> destination,
+            ResolutionContext context)
         {
             if (source == null) return null;
-            var vm = source.Items.Select(m => context.Mapper.Map<TMapFrom, TMapTo>(m)).ToList();
+            var vm = source.Items.Select(
+                m => context.Mapper.Map<TMapFrom, TMapTo>(m)).ToList();
 
 
-            var pagedList = PagedList.From<TMapTo, TMapFrom>(source, (con) => context.Mapper.Map<IEnumerable<TMapTo>>(con));
+            var pagedList = PagedList.From<TMapTo, TMapFrom>(
+                source, (con) => context.Mapper.Map<IEnumerable<TMapTo>>(con));
             // var pagedList = vm.ToPagedList(source.PageIndex, source.PageSize);
             return pagedList;
         }
