7321e3a86b7281317f7e3e9b49d008bb26402372
Сергей Захаров <sergei86zahar@mail.ru>
2021-08-20 19:05:01 +0700

    1

--- Calabonga.IdentityModule1/Calabonga.IdentityModule1.Web/Controllers/AccountController.cs
+++ Calabonga.IdentityModule1/Calabonga.IdentityModule1.Web/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using Calabonga.IdentityModule1.Web.ViewModels.AccountViewModels;
 using Calabonga.OperationResults;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
@@ -35,6 +36,9 @@ namespace Calabonga.IdentityModule1.Web.Controllers
         [ProducesResponseType(200, Type = typeof(OperationResult<UserProfileViewModel>))]
         public async Task<ActionResult<OperationResult<UserProfileViewModel>>> Register([FromBody] RegisterViewModel model)
         {
+            HttpResponse resp = HttpContext.Response;
+            string cookie = GetCookieValueFromResponse(resp, ".AspNetCore.Cookie");
+
             return Ok(await _mediator.Send(new RegisterRequest(model), HttpContext.RequestAborted));
         }
 
@@ -47,6 +51,25 @@ namespace Calabonga.IdentityModule1.Web.Controllers
         public async Task<ActionResult<OperationResult<UserProfileViewModel>>> Profile()
         {
             return await _mediator.Send(new ProfileRequest(), HttpContext.RequestAborted);
+        }
+
+        /// <summary>
+        /// Получение токена из Cookie
+        /// </summary>
+        /// <param name="response"></param>
+        /// <param name="cookieName"></param>
+        /// <returns></returns>
+        private string GetCookieValueFromResponse(HttpResponse response, string cookieName)
+        {
+            foreach (var headers in response.Headers.Values)
+                foreach (var header in headers)
+                    if (header.StartsWith($"{cookieName}="))
+                    {
+                        var p1 = header.IndexOf('=');
+                        var p2 = header.IndexOf(';');
+                        return header.Substring(p1 + 1, p2 - p1 - 1);
+                    }
+            return null;
         }
     }
 }
